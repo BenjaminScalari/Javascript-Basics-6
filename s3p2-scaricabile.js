d6c0817b6e3809ab828eb95beb3d8501688d6431
BenjaminScalari <157407147+BenjaminScalari@users.noreply.github.com>
2024-02-26 19:49:55 +0100

    second commit
    svolti esercizi js, bisogna ancora terminare con le media queries per renderlo un sito per smartphone

--- s3p2-scaricabile.js
+++ s3p2-scaricabile.js
@@ -14,12 +14,12 @@ ESERCIZIO 5: Aggiungi un footer con il nome e l'indirizzo del tuo negozio
 ESERCIZIO 6: Aggiungi un campo testuale in cui l'utente può lasciare un commento su un prodotto (al momento non serve inserire nessuna "vera" funzionalità di POST/salvataggio!)
 
 //ESERCIZI IN JS
-// ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
-// ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
-// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel footer in un altro, fittizio
-// ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
-// ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
-// ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
+ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
+ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
+ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel footer in un altro, fittizio
+ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
+ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
+ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
 
 
 
---
+++ script.js
@@ -0,0 +1,71 @@
+// CAMBIARE TITOLO
+let h1 = document.getElementById("titolo");
+
+function cambiaTitolo() {
+  if (h1.innerText === "Pokémon Market Online") {
+    h1.innerText = "PMO";
+  } else {
+    h1.innerText = "Pokémon Market Online";
+  }
+}
+h1.addEventListener("click", cambiaTitolo);
+
+// DARK MODE
+let darkModeButton = document.getElementById("darkModeButton");
+function toggleDarkMode() {
+  let isDarkMode = document.body.classList.contains("dark-mode");
+  if (isDarkMode) {
+    // DISATTIVA
+    document.body.classList.remove("dark-mode");
+    darkModeButton.textContent = "Attiva Dark Mode";
+  } else {
+    // ATTIVA
+    document.body.classList.add("dark-mode");
+    darkModeButton.textContent = "Disattiva Dark Mode";
+  }
+}
+darkModeButton.addEventListener("click", toggleDarkMode);
+
+// CAMBIARE FOOTER
+function changeFooterAddress(newAddress) {
+  let footerAddressElement = document.getElementById("footerAddress");
+  footerAddressElement.textContent = newAddress;
+}
+changeFooterAddress("@ 2024 Devon S.p.A. Ferrugipoli.");
+
+// AGGIUNGERE CLASSE AI LINK
+let links = document.querySelectorAll("#cards-container a");
+links.forEach(function(link) {
+  link.classList.add("classe-x-esercizio");
+});
+
+// TASTO NASCONDI-IMMAGINI
+function toggleImageVisibility() {
+  let images = document.querySelectorAll("#cards-container img");
+  images.forEach(function(image) {
+    image.classList.toggle("hidden");
+  });
+}
+document.getElementById("imgButton").addEventListener('click', toggleImageVisibility);
+
+// PREZZI CHE CAMBIANO COLORE
+// GENERATORE DI COLORI RGB
+function getRandomRGB() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  let color = "rgb(" + r + "," + g + "," + b + ")";
+  return color;
+}
+let prices = document.querySelectorAll("h4");
+
+prices.forEach(function(price) {
+  // CAMBIA COLORE TESTO
+    price.addEventListener("mouseenter", function() {
+        price.style.color = getRandomRGB();
+    });
+    // RIPRISTINA COLORE TESTO
+    price.addEventListener("mouseleave", function() {
+      price.style.color = "black";
+  });
+});
